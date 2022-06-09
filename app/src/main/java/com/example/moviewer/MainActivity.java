1c09e8afc82431ca45f24503b95aa02a925f94c0
maevym <83373618+maevym@users.noreply.github.com>
2022-06-10 05:53:02 +0700

    add_home

--- app/src/main/java/com/example/moviewer/MainActivity.java
+++ app/src/main/java/com/example/moviewer/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.moviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button hi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hi=findViewById(R.id.hi);
+        hi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==hi){
+            Intent i = new Intent(this, Home.class);
+            startActivity(i);
+        }
     }
 }

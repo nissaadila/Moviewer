59c35552d9e9a5113b02408a6d5a02833ce59b24
maevym <83373618+maevym@users.noreply.github.com>
2022-06-11 23:59:03 +0700

    add_fragment_layout_profile

--- app/src/main/java/com/example/moviewer/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/moviewer/Fragments/ProfileFragment.java
@@ -1,66 +1,116 @@
 package com.example.moviewer.Fragments;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.moviewer.LoginActivity;
 import com.example.moviewer.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfileFragment extends Fragment {
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ProfileFragment extends Fragment implements View.OnClickListener {
+    EditText editTextUsername, editTextEmail;
+    Button editUsername, editEmail, saveUsername, saveEmail, editPhoto, logOut;
+    TextView username, email;
+    ImageView profile;
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Bitmap bitmap = null;
+    byte image[];
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        editTextUsername = view.findViewById(R.id.EditTextUsername);
+        editTextEmail = view.findViewById(R.id.EditTextEmail);
+        editUsername = view.findViewById(R.id.BtnEditUsername);
+        editEmail = view.findViewById(R.id.BtnEditEmail);
+        saveUsername = view.findViewById(R.id.BtnUsernameSave);
+        saveEmail = view.findViewById(R.id.BtnEmailSave);
+        editPhoto = view.findViewById(R.id.BtnProfilePic);
+        logOut = view.findViewById(R.id.BtnLogOut);
+        username = view.findViewById(R.id.TextUsername);
+        email = view.findViewById(R.id.TextEmail);
+        profile = view.findViewById(R.id.profilePic);
+
+        editUsername.setOnClickListener(this);
+        editEmail.setOnClickListener(this);
+        saveEmail.setOnClickListener(this);
+        saveUsername.setOnClickListener(this);
+        editPhoto.setOnClickListener(this);
+        logOut.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == editUsername) {
+            editTextUsername.setVisibility(View.VISIBLE);
+            saveUsername.setVisibility(View.VISIBLE);
+            editUsername.setVisibility(View.GONE);
+            username.setVisibility(View.GONE);
+        } else if (view == saveUsername) {
+            editTextUsername.setVisibility(View.GONE);
+            saveUsername.setVisibility(View.GONE);
+            editUsername.setVisibility(View.VISIBLE);
+            username.setVisibility(View.VISIBLE);
+        } else if (view == editEmail) {
+            editTextEmail.setVisibility(View.VISIBLE);
+            saveEmail.setVisibility(View.VISIBLE);
+            editEmail.setVisibility(View.GONE);
+            email.setVisibility(View.GONE);
+        } else if (view == saveEmail) {
+            editTextEmail.setVisibility(View.GONE);
+            saveEmail.setVisibility(View.GONE);
+            editEmail.setVisibility(View.VISIBLE);
+            email.setVisibility(View.VISIBLE);
+        } else if (view == logOut) {
+            Intent i = new Intent(getActivity(), LoginActivity.class);
+            startActivity(i);
+        } else if (view == editPhoto) {
+            chooseProfilePhoto();
+        }
+    }
+
+    private void chooseProfilePhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,0);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode== Activity.RESULT_OK && data !=null) {
+            Uri selectedImage = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), selectedImage);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                image = baos.toByteArray();
+                profile.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }

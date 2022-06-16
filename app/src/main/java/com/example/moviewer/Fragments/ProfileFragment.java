a82681103ee47d9cd6d0c3af84888a5502bdd017
maevym <83373618+maevym@users.noreply.github.com>
2022-06-17 00:24:25 +0700

    hapus_profilepic

--- app/src/main/java/com/example/moviewer/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/moviewer/Fragments/ProfileFragment.java
@@ -1,25 +1,15 @@
 package com.example.moviewer.Fragments;
 
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.database.Cursor;
 
 import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,23 +18,16 @@ import com.example.moviewer.LoginActivity;
 import com.example.moviewer.Models.User;
 import com.example.moviewer.R;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
     EditText editTextUsername, editTextEmail;
-    Button editUsername, editEmail, saveUsername, saveEmail, editPhoto, logOut;
+    Button editUsername, editEmail, saveUsername, saveEmail, logOut;
     TextView username, email;
-    ImageView profile;
 
     UserHelper uh;
     User curr_user;
     private final int curr_id;
 
-    Bitmap bitmap = null;
-    byte image[];
 
     String curr_email, curr_password, curr_username;
     String new_email, new_username;
@@ -64,17 +47,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         editEmail = view.findViewById(R.id.BtnEditEmail);
         saveUsername = view.findViewById(R.id.BtnUsernameSave);
         saveEmail = view.findViewById(R.id.BtnEmailSave);
-        editPhoto = view.findViewById(R.id.BtnProfilePic);
         logOut = view.findViewById(R.id.BtnLogOut);
         username = view.findViewById(R.id.TextUsername);
         email = view.findViewById(R.id.TextEmail);
-        profile = view.findViewById(R.id.profilePic);
 
         editUsername.setOnClickListener(this);
         editEmail.setOnClickListener(this);
         saveEmail.setOnClickListener(this);
         saveUsername.setOnClickListener(this);
-        editPhoto.setOnClickListener(this);
         logOut.setOnClickListener(this);
         uh = new UserHelper(getActivity());
         curr_user = uh.getUserData(curr_id);
@@ -148,30 +128,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         } else if (view == logOut) {
             Intent i = new Intent(getActivity(), LoginActivity.class);
             startActivity(i);
-        } else if (view == editPhoto) {
-            chooseProfilePhoto();
-        }
-    }
-
-    private void chooseProfilePhoto() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent,0);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode== Activity.RESULT_OK && data !=null) {
-            Uri selectedImage = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), selectedImage);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                image = baos.toByteArray();
-                profile.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
